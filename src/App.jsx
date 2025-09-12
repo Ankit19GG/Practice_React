448ff3a9afbc26b5595334de34836791b36c4993
Ankit Karak <ankitkarak19@gmail.com>
2025-09-13 03:04:51 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Accordian from './components/accordian'
 import './app.css'
+import ColorGenerator from './components/Color generator/Index'
 
 function App() {
 
   return (
     <>
       <Accordian/>
+      <ColorGenerator/>
     </>
   )
 }
