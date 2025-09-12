46a077d8a55d8307ac9d66f570adc7045a39ed56
Ankit Karak <ankitkarak19@gmail.com>
2025-09-13 03:05:23 +0530

    2

---
+++ src/components/Color generator/index.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+
+function rgb(){
+    return Math.floor(Math.random()*256);
+};
+
+function ColorGenerator(){
+    const [code,setCode] = useState("#0084ff");
+    const [typeofCode,settypeofCode] = useState(true);
+
+    useEffect(()=>{
+        if(typeofCode){
+            const code =Math.floor(Math.random()*16777215).toString(16);
+            code.length<6?code="0"+code:null;
+            setCode("#"+code);
+        }
+        else{
+            setCode("rgb("+rgb()+","+rgb()+","+rgb()+")");
+        }
+    },[typeofCode])
+
+    return(
+        <div style={{ backgroundColor: code }} className={`mt-10 p-10 h-150`}>
+           <div>
+            <button onClick={()=>settypeofCode(!typeofCode)}>Create {typeofCode?"RGB":"HEX"} Color</button>
+            <button onClick={()=>{
+                if (typeofCode){
+                    const code =Math.floor(Math.random()*16777215).toString(16);
+                    code.length<6?code="0"+code:null;
+                    setCode("#"+code);
+                }
+                else{
+                    setCode("rgb("+rgb()+","+rgb()+","+rgb()+")");   
+                };
+            }}>Generate Random color</button>
+           </div>
+           <p className="text-6xl font-medium my-10">{typeofCode?"HEX":"RGB"} Color</p>
+           <p className="text-8xl font-medium my-15">{code}</p>
+        </div>
+    )
+}
+
+export default ColorGenerator;
